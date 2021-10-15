dfcd157d822e21f299245aa67818bd206335b7ee
JhonatanAlva <jloola@miumg.edu.gt>
2021-10-15 12:12:22 -0600

    init files

---
+++ ProgramBurbuja.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace vcs_CS
+{
+    class Program
+    {
+        static int[] numbers = new int[] { 10, 2, 3, 6, 9, 1, 4, 5, 7, 8 };
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicio");
+            Show();
+
+            for (int i=0; i<numbers.Length; i++)
+            {
+                for (int j=0; j<numbers.Length-1; j++)
+                {
+                    if(numbers[j] > numbers[j + 1])
+                    {
+                        int aux = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = aux;
+                    }
+                }
+                Show();
+            }
+            Show();
+        }
+
+        public static void Show()
+        {
+            foreach (var number in numbers)
+            {
+                Console.Write(number + ",");
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ ProgramIncer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OrdenacionInsercion
+{
+    using System;
+    using System.Linq;
+ 
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa los numeros a ordenar separados por comas(,) y presiona Enter.");
+            int[] numero = Console.ReadLine().Replace(" ","").Split(',').Select(x => Convert.ToInt32(x)).ToArray();
+            Console.WriteLine();
+            Console.WriteLine("ordenado:");
+            Console.WriteLine(string.Join(",", Ordenar(numero)));
+            Console.WriteLine();
+            Console.WriteLine("Presiona una tecla para salir.");
+            Console.ReadKey();
+        }
+        private static int[] Ordenar(int[] numero)
+        {
+            int auxiliar;
+ 
+            for (int i = 1; i < numero.Length; i++)
+            {
+                auxiliar = numero[i];
+ 
+                for (int j = i - 1; j >= 0 && numero[j] > auxiliar; j--)
+                {
+                    numero[j + 1] = numero[j];
+                    numero[j] = auxiliar;
+                }
+            }
+ 
+            return numero;
+        }
+    }
+}
